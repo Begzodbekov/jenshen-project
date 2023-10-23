3a6831822b672429c73983650159a3175bc154ec
sarvaralimovq <sarvaralimovq@gmail.com>
2023-10-23 15:46:15 +0500

    sg

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,11 @@ function Header() {
   return (
     <div className='Header'>
         <div className="container">
-          
+            <nav className='nav'>
+                <div className="logo">
+                  
+                </div>
+            </nav>
         </div>
     </div>
   )
