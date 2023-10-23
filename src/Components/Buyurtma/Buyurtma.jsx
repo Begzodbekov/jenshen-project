eca6ed2ae69aded1850cc5cd68c4873ed489d88b
Begzodbekov <begzodxusanboyev2006@gmail.com>
2023-10-23 15:37:15 +0500

    fayllar tayyor

---
+++ src/Components/Buyurtma/Buyurtma.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Buyurtma() {
+  return (
+    <div>Buyurtma</div>
+  )
+}
+
+export default Buyurtma
---
+++ src/Components/Hususiyat/Hususiyat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hususiyat() {
+  return (
+    <div>Hususiyat</div>
+  )
+}
+
+export default Hususiyat
