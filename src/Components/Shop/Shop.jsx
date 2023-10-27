0da638a74dfa6204f27326d8dd17c043b33de1ae
sarvaralimovq <sarvaralimovq@gmail.com>
2023-10-27 16:34:54 +0500

    sfsa

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,30 +1,26 @@
 import React from 'react'
 import './Shop.scss'
-import shop__left from '../../img/Shop__left.png'
+// import shop__left from '../../img/Shop__left.png'
 import shop__leftSet from '../../img/shop__leftSet.png'
 function Shop() {
   return (
     <div className='Shop'>
       <div className="container">
           <div className="shop__inner">
-              <div className="shop__left">
-                  <img className='shop__leftImg' src={shop__left} alt="shop__left" />
-                  <img className='shop__leftSetImg' src={shop__leftSet} alt="shop__leftSet" />
-                  <span className='shop__bottom'>
-                    <span className='shop__bottom__item'><p className='shop__name'>Qizil jen shen</p>
-                    <p className='shop__price'>1 199 000</p></span>
-                  </span>
-              </div>
-              <div className="shop__right">
-                <img className='shop__leftImg' src={shop__left} alt="shop__left" />
-                <img className='shop__leftSetImg' src={shop__leftSet} alt="shop__leftSet" />
-                <span className='shop__bottom'>
-                   <span className='shop__bottom__item'>
-                   <p className='shop__name'>Qizil jen shen</p>
-                    <p className='shop__price'>1 199 000</p>
-                   </span>
-                  </span>
-              </div>
+            <div className="shop_left">
+             <img className='shop__leftSet' src={shop__leftSet} alt="img" />
+             <span>
+              <p>Qizil jen shen</p>
+              <p>1 199 000</p>
+             </span>
+            </div>
+            <div className="shop_right">
+              <img className='shop__leftSet' src={shop__leftSet} alt="img" />
+              <span>
+              <p>Qizil jen shen</p>
+              <p>1 199 000</p>
+             </span>
+            </div>
           </div>
       </div>
     </div>
